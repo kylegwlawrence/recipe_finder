e74570ad7839c783dcf497881e016079752813e1
Kyle Lawrence <kylelawrence@Aspens-MacBook-Air.local>
2024-03-10 22:59:45 -0700

    read recipes from txt file

--- generate_recipes.py
+++ generate_recipes.py
@@ -20,6 +20,8 @@ def get_recipes_by_ingredients(apikey, ingredients:str, num_recipes_returned:int
 
         r = requests.get(url)
         recipes_contents = r.text
+        time.sleep(1.5)
+    
 
     return recipes_contents
 
@@ -28,21 +30,21 @@ def get_recipe_instructions(apikey, recipe_id:int, use_sample='no'):
     if use_sample.lower()=='yes':
         with open('sample_instructions.txt') as f:
             instructions_contents = f.readlines()
-        #print(contents)
     elif use_sample.lower()=='no':
         # format ingredients string for url
         url = f'https://api.spoonacular.com/recipes/{recipe_id}/analyzedInstructions?apiKey={apikey}'
         r = requests.get(url)
         instructions_contents = r.text
+        time.sleep(1.5)
 
     return instructions_contents
 
-def instructions(instructions_contents) -> list[str]:
+def parse_instructions(instructions_content) -> list[str]:
     """Generate readable instructions by parsing instructions response"""
     recipe_instructions = []
-    for entry in instructions_contents:
-        data = json.loads(entry)
-        for instruction_step in data[0].get('steps'):
+    data = json.loads(instructions_content)
+    for entry in data:
+        for instruction_step in entry.get('steps'):
             string = f"Step {instruction_step.get('number')}: {instruction_step.get('step')}"
             recipe_instructions.append(string)
     return recipe_instructions
@@ -50,23 +52,22 @@ def instructions(instructions_contents) -> list[str]:
 def parse_recipes(recipes_contents) -> list[dict]:
     """return list of tuples with id and recipe name"""
     recipes = []
-    for entry in recipes_contents:
-        data = json.loads(entry)
-        for recipe in data:
-            d = {}
-            d['id'] = recipe.get('id')
-            d['name'] = recipe.get('title')
-            ingredients = []
+    data = json.loads(recipes_contents)
+    for recipe in data:
+        d = {}
+        d['id'] = recipe.get('id')
+        d['name'] = recipe.get('title')
+        ingredients = []
 
-            for ingredient in recipe.get('usedIngredients'):
-                ingredients.append(ingredient.get('original'))
-            for ingredient in recipe.get('unusedIngredients'):
-                ingredients.append(ingredient.get('original'))
-            for ingredient in recipe.get('missedIngredients'):
-                ingredients.append(ingredient.get('original'))
-            d['ingredients'] = ingredients
+        for ingredient in recipe.get('usedIngredients'):
+            ingredients.append(ingredient.get('original'))
+        for ingredient in recipe.get('unusedIngredients'):
+            ingredients.append(ingredient.get('original'))
+        for ingredient in recipe.get('missedIngredients'):
+            ingredients.append(ingredient.get('original'))
+        d['ingredients'] = ingredients
 
-            recipes.append(d)
+        recipes.append(d)
     return recipes
 
 def build_recipes_from_ingredients(apikey, ingredients:str, num_recipes_returned:int, use_sample='yes') -> list[dict]:
@@ -76,19 +77,22 @@ def build_recipes_from_ingredients(apikey, ingredients:str, num_recipes_returned
     for recipe in recipes:
         recipe_id = recipe.get('id')
         instructions_contents = get_recipe_instructions(apikey, recipe_id, use_sample)
-        recipe_instructions = instructions(instructions_contents)
+        recipe_instructions = parse_instructions(instructions_contents)
         recipe['instructions'] = recipe_instructions
 
-    return recipes
-
-if __name__ == '__main__':
-    apikey = 'a5e1c05d6d4442ad88338e361aafc1f6'
-    ingredients = 'lamb, potato, olive oil'
-    num_recipes_returned = 3
-    recipes = build_recipes_from_ingredients(apikey, ingredients, num_recipes_returned, use_sample='yes')
-    
     # write recipes to file
     timestr = time.strftime("%Y%m%d-%H%M%S")
     ingredients = ingredients.replace(', ','-').replace(' ','')
-    with open(f'recipes/{ingredients}_{num_recipes_returned}_{timestr}.txt', 'w') as f:
+    file_name = f'recipes/{ingredients}_{num_recipes_returned}_{timestr}.txt'
+    with open(file_name, 'w') as f:
         json.dump(recipes, f)
+
+    print(f'Recipes saved to {file_name}')
+
+if __name__ == '__main__':
+    apikey = 'a5e1c05d6d4442ad88338e361aafc1f6'
+    ingredients = 'potato, bamboo, coconut, tofu'
+    num_recipes_returned = 5
+    use_sample = 'no'
+
+    build_recipes_from_ingredients(apikey, ingredients, num_recipes_returned, use_sample)

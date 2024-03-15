d360a837a75342fa37fec1451d7bbd4dd590f9e0
Kyle Lawrence <kylelawrence@Aspens-MacBook-Air.local>
2024-03-15 16:12:39 -0700

    create markdown of multiple recipes using the classes

--- my_classes.py
+++ my_classes.py
@@ -10,18 +10,20 @@ import spoonacular_api as spa
 def endpoint_base() -> str:
     return 'https://api.spoonacular.com'
 
-def apikey() -> str:
-    return 'a5e1c05d6d4442ad88338e361aafc1f6'
+def api_key() -> str:
+    api_key = os.environ.get("SPOON_API_TOKEN")
+    return api_key
 
 class Recipe:
     def __init__(self, recipe_id:str=None):
         """
         :param recipe_id: the spoonacular recipe id as a string
         """
-        self.apikey = apikey()
+        self.api_key = api_key()
+        root_endpoint = endpoint_base()
 
         self.recipe_id = recipe_id
-        self.url_base = f'{endpoint_base()}/recipes/{self.recipe_id}'
+        self.url_base = f'{root_endpoint}/recipes/{self.recipe_id}'
         self.info = None
         self.instructions = None
     
@@ -31,7 +33,7 @@ class Recipe:
         such as ingredients, nutrition, diet and allergen information, etc.
         """
         str_include_nutrition = str(includeNutrition).lower()
-        url = f'{self.url_base}/information?includeNutrition={str_include_nutrition}&apiKey={self.apikey}'
+        url = f'{self.url_base}/information?includeNutrition={str_include_nutrition}&apiKey={self.api_key}'
         response = requests.get(url)
         self.info = json.loads(response.text)
         return self.info
@@ -40,7 +42,11 @@ class Recipe:
         """
         Extract the ingredients from self.info
         """
-        self.ingredients = None
+        self.ingredients = []
+        for ingredient in self.info.get('extendedIngredients'):
+            original = ingredient.get('original')
+            self.ingredients.append(original)
+        return self.ingredients
     
     def get_instructions_from_info(self) -> list[dict]:
         """Extract the instructions from self.info
@@ -62,22 +68,25 @@ class Recipe:
         """
         Get the recipe instructions from the api endpoint analyzedInstructions
         """
-        url = f'{self.url_base}/analyzedInstructions?apiKey={self.apikey}'
+        url = f'{self.url_base}/analyzedInstructions?apiKey={self.api_key}'
         response = requests.get(url)
         self.instructions = json.loads(response.text)[0]
         return self.instructions
     
-    def get_equipment_from_instructions(self) -> list[dict]:
+    def get_equipment_from_instructions(self) -> list:
         """
-        Use result from get_instructions() to store equipment info in a list of
+        Use result from get_instructions() to store equipment info in a list.
+        Optionally store the step number associated with the equipment.
         """
         if self.instructions == None:
             raise AttributeError('No instructions exists. Please create info using Recipe.get_info() or pass in self.info')
-        self.list_equipment = []
+        self.list_tools = []
         for instruction in self.instructions:
-            equipment = instruction.get('equipment')[0]
-            self.list_equipment.append(equipment)
-        return self.list_equipment
+            equipment = instruction.get('equipment')
+            for tool in equipment:
+                tool_name = tool.get('name')
+                self.list_tools.append(tool_name)
+        return self.list_tools
         
     def get_name(self) -> str:
         self.name = self.info.get('title')
@@ -87,36 +96,30 @@ class Recipe:
         self.servings = self.info.get('servings')
         return self.servings
     
-    def get_time(self):
+    def get_time(self) -> int:
         self.time = self.info.get('readyInMinutes')
+        return self.time
 
     def get_taste_from_api(self) -> dict:
-        url = f'{self.url_base}/tasteWidget.json?apiKey={self.apikey}'
+        url = f'{self.url_base}/tasteWidget.json?apiKey={self.api_key}'
         response = requests.get(url)
         self.taste = json.loads(response.text)
         print(self.taste)
         print(type(self.taste))
         return self.taste
     
-    def to_markdown(self):
-        """
-        Either write formatting logic here or create a MarkdownRecipe class to format then save here.
-        Simplest to format here since only applying to one class of object
-        """
-        self.recipe_in_markdown = RecipeMarkdown(self.ingredients, self.instructions, self.list_equipment, self.name, self.servings, self.time)
-        
 class RecipesInfoBulk:
     """
     Loop over the results of this using the Recipe class to generate attributes
     This endpoint costs 1 point for the first recipe and 0.5 points per additional recipe.
     """
-    apikey = apikey()
+    api_key = api_key()
     def __init__(self, recipe_ids:str) -> None:
         """
         :param recipe_ids: a comma-separated string of recipe ids
         """
         self.recipe_ids = recipe_ids
-        self.url = f'{endpoint_base()}/recipes/informationBulk?ids={self.recipe_ids}&includeNutrition=true&apiKey={self.apikey}'
+        self.url = f'{endpoint_base()}/recipes/informationBulk?ids={self.recipe_ids}&includeNutrition=true&apiKey={self.api_key}'
     
     def get_info(self) -> list[dict]:
         response = requests.get(self.url)
@@ -130,7 +133,7 @@ class SearchRecipesByIngredients:
     Although this endpoint comes with equipment, instructions have to be called separately per id.
     Instead pull all recipe info at once contained within same api response to obtain
     """
-    apikey = apikey()
+    api_key = api_key()
     def __init__(self, ingredients:str, num_recipes:int, rank_ingredients:str='maximize_used') -> None:
         self.ingredients = ingredients
         url_ingredients = ingredients.replace(', ',',+')
@@ -144,7 +147,7 @@ class SearchRecipesByIngredients:
                 self.rank = 1
             else:
                 self.rank = 2
-        self.url = f'{endpoint_base()}/recipes/findByIngredients?apiKey={self.apikey}&ingredients={self.ingredients}&number={self.num_recipes}&ranking={self.rank}'
+        self.url = f'{endpoint_base()}/recipes/findByIngredients?apiKey={self.api_key}&ingredients={self.ingredients}&number={self.num_recipes}&ranking={self.rank}'
     
     def get_recipes(self) -> list[dict]:
         response = requests.get(self.url)
@@ -170,40 +173,89 @@ class SearchRecipesByIngredients:
         return self.cat_ingredients
 
 class RecipeMarkdown:
-    # make a class to build a recipe which would use either one recipe or RecipesByIngredients
-    def to_markdown(self, recipe_info):
+    def __init__(self, name:str, servings:int, time:int, ingredients:list, instructions, equipment:list, file_name:str) -> None:
         """
-        Create a formatted markdown file with the 
+        File name must include realtive path if not in root
         """
-        if self.data==None:
-            self.get_recipes()
-        title = f'Recipes for {self.ingredients}'
-        mdFile = MdUtils(self.file_name, title)
+        self.name = name
+        self.servings = servings
+        self.time = time
+        self.ingredients = ingredients
+        self.instructions = instructions
+        self.equipment = equipment
+        self.file_name = file_name
+
+    def to_markdown(self):
+        """
+        Write one recipe to a markdown file.
+        """
+        mdFile = MdUtils(self.file_name, self.name)
+
+        # prep time and num servings
+        mdFile.new_line(f"Ready in {self.time} mins | Servings: {self.servings}")
+
+        # list ingredients
+        mdFile.new_header(level=3, title=f'Ingredients', add_table_of_contents='n')
+        for ingredient in self.ingredients:
+            mdFile.new_line(f"  - {ingredient}")
+
+        # list equipment
+        mdFile.new_header(level=3, title=f'Equipment', add_table_of_contents='n')
+        for tool in self.equipment:
+            mdFile.new_line(f"  - {tool}")
+
+        # list instructions
+        mdFile.new_header(level=3, title=f'Instructions', add_table_of_contents='n')
+        for instruction in self.instructions:
+            mdFile.new_line(f"  - Step {instruction.get('number')}: {instruction.get('step')}\n")
+
+        # save locally
+        try:
+            mdFile.create_md_file()
+        except:
+            print(f'Something went wrong saving {self.file_name} locally')
+
+class ManyRecipesMarkdown:
+    def __init__(self, recipe_markdown_instances:list, file_name:str, searched_ingredients:str) -> None:
+        """
+        takes in a list of RecipeMarkdown instances and combines them into one file.
+        useful for creating a file of recipes that are found through SearchRecipesByIngredients
+        """
+        self.markdown_recipes = recipe_markdown_instances
+        self.file_name = file_name
+        self.searched_ingredients = searched_ingredients
+
+    def to_markdown(self): 
+        """
+        itereate over the list of RecipeMarkdown objects and create one markdown file
+        """
+        title = f'Recipes for {self.searched_ingredients}'
+        mdFile = MdUtils(file_name=self.file_name, title=title)
+
         # list the recipes at the top of the document
-        mdFile.new_header(level=3, title=title, add_table_of_contents='n')
-        for r in self.data:
-            r_name = r.get('name')
-            mdFile.new_line(f'  - {r_name}')
+        for md_recipe in self.markdown_recipes:
+            mdFile.new_line(f'  - {md_recipe.name}')
+
         # add one section for each recipe
-        for r in self.data:
-            r_name = r.get('name')
-            # ingredients
-            mdFile.new_header(level=2, title=f'{r_name}', add_table_of_contents='n')
+        for md_recipe in self.markdown_recipes:
+            mdFile.new_header(level=2, title=md_recipe.name, add_table_of_contents='n')
+            # prep time and num servings
+            mdFile.new_line(f"Ready in {md_recipe.time} mins | Servings: {md_recipe.servings}")
+
+            # list ingredients
             mdFile.new_header(level=3, title=f'Ingredients', add_table_of_contents='n')
-            mdFile.new_header(level=4, title=f'Used ingredients', add_table_of_contents='n')
-            for ingredient in r.get('usedIngredients'):
-                mdFile.new_line(f"  - {ingredient.get('name')}")
-            mdFile.new_header(level=4, title=f'Missed ingredients', add_table_of_contents='n')
-            for ingredient in r.get('missedIngredients'):
-                mdFile.new_line(f"  - {ingredient.get('name')}")
-            if len(r.get('unusedIngredients'))>0:
-                mdFile.new_header(level=4, title=f'Unused ingredients', add_table_of_contents='n')
-                for ingredient in r.get('unused_ingredients'):
-                    mdFile.new_line(f"  - {ingredient.get('name')}")
-            # instructions
-            mdFile.new_header(level=3, title=f'Directions', add_table_of_contents='n')
-            for instruction in r.get('instructions'):
-                mdFile.new_line(f'  - {instruction}')
+            for ingredient in md_recipe.ingredients:
+                mdFile.new_line(f"  - {ingredient}")
+
+            # list equipment
+            mdFile.new_header(level=3, title=f'Equipment', add_table_of_contents='n')
+            for tool in md_recipe.list_tools:
+                mdFile.new_line(f"  - {tool}")
+
+            # list instructions
+            mdFile.new_header(level=3, title=f'Instructions', add_table_of_contents='n')
+            for instruction in md_recipe.instructions:
+                mdFile.new_line(f"  - Step {instruction.get('number')}: {instruction.get('step')}\n")
         try:
             mdFile.create_md_file()
         except:
@@ -226,27 +278,25 @@ class RecipeMarkdown:
         self.github_link = f'https://github.com/kylegwlawrence/recipe_finder/blob/main/{self.file_name}'
         if open_browser:
             urllib.request.urlopen(self.github_link)
-    
+
 if __name__ == '__main__':
-    search = SearchRecipesByIngredients('chicken, lemon', 2)
-    search.get_recipes()
-    list_ids = search.get_recipe_ids()
-    string_ids = ', '.join(str(i) for i in map(str, list_ids))
-    bulk = RecipesInfoBulk(string_ids)
-    r_infos = bulk.get_info()
-    all_recipes = []
-    for recipe_info in r_infos:
-        id = recipe_info.get('id')
-        single_recipe = Recipe(id)
-        single_recipe.info = recipe_info
-        all_recipes.append(single_recipe) # list of Recipe objects
+    # search for ingredients, get recipe ids, pass to Bulk recipe info, pass to Recipe, pass to manyMarkdown recipes
+    search_ingredients = 'lamb, cabbage, orange, ginger'
+    recipes_by_ingredients = SearchRecipesByIngredients(ingredients=search_ingredients, num_recipes=5)
+    recipes_by_ingredients.get_recipes()
+    recipes_ids = recipes_by_ingredients.get_recipe_ids()
 
-
-if __name__ == '__main':
-    r = Recipe(recipe_id = 715438)
-    info = r.get_info()
-    instructions = r.get_instructions_from_info()
-    #equipment = r.get_equipment_from_instructions()
-    print(info)
-    print(type(info))
-    
+    bulk_info = RecipesInfoBulk(recipes_ids).get_info()
+    recipe_list = []
+    for recipe_info in bulk_info:
+        r = Recipe()
+        r.info = recipe_info
+        r.get_ingredients_from_info()
+        r.get_instructions_from_info()
+        r.get_equipment_from_instructions()
+        r.get_name()
+        r.get_servings()
+        r.get_time()
+        recipe_list.append(r)
+    md = ManyRecipesMarkdown(recipe_list, file_name = f'Recipes for {search_ingredients}.md', searched_ingredients=search_ingredients)
+    md.to_markdown()
--- spoonacular_api.py
+++ spoonacular_api.py
@@ -6,7 +6,7 @@ import os
 class ApiClient:
     # what does an api client do?
     def __init__(self) -> None:
-        # retrieve the api token
+        # retrieve the api token from environment variables
         self.api_key=os.environ.get("SPOON_API_TOKEN")
         # creates a root url
         self.root_endpoint = 'https://api.spoonacular.com'
@@ -14,11 +14,9 @@ class ApiClient:
     def post_request(self, url, api_key):
         print('Use example from this stackoverflow response: https://stackoverflow.com/questions/65265786/python-call-rest-api-to-get-data-from-url')
         session = requests.Session()
-        url = "https://hpe.sysnergy.com/rest/login-sessions"
         credentials = {"apiKey": api_key}
         headers = {"accept": "application/json", 
-                "content-type": "application/json", 
-                "x-api-version": "120", 
+                "content-type": "application/json"
                 }
         response = session.post(url, headers=headers, json=credentials, verify=False)
 
@@ -28,10 +26,35 @@ class ApiClient:
     def get_request(self, url, session_id):
         url = "https://hpe.sysnergy.com/rest/resource-alerts"
         headers = {"accept": "application/json", 
-                "content-type": "text/csv", 
-                "x-api-version": "2", 
+                "content-type": "application/json", 
                 "auth": session_id, 
                 }
         session = requests.Session()
         response = session.get(url, headers=headers, verify=False)
         return response
+    
+class ApiEndpoint:
+    def __init__(self) -> None:
+        pass
+
+    def recipe_information(self, recipe_id, include_nutrition:bool=True):
+        """
+        Use a recipe id to get full information about a recipe, 
+        such as ingredients, nutrition, diet and allergen information, etc.
+        """
+        #instantiate api client
+        api_client = ApiClient()
+        include_nutrition = str(include_nutrition).lower()
+        # construct the url
+        url = f'{api_client.root_endpoint}/information?includeNutrition={include_nutrition}'
+        
+        # make post request
+        session_id = api_client.post_request(url=url, api_key=api_client.api_key)
+        # make get request using session id
+        response = api_client.get_request(url, session_id)
+        self.info = json.loads(response.text)
+        return self.info
+
+if __name__ == '__main__':
+    api = ApiClient()
+    print(api.post_request(url = '', api_key=api.api_key))
